30f24e1765ec4e155012a9b69010f5b19ccdda7b
paphuc <29089718+paphuc@users.noreply.github.com>
2022-11-17 22:14:58 +0700

    Add middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>,
+document.getElementById('root')
+)
